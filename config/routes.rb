9cdee2fe329444eba9e3df432687b63ccbf9184f
Anas Abubakar <anasabubakar2015@gmail.com>
2022-11-16 08:50:44 +0100

    Implements list all foods

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'foods#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :foods, except: [:edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
