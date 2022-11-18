05bc3d2fb3393285f468b1d32f47149ce1b795b3
Anas Abubakar <anasabubakar2015@gmail.com>
2022-11-18 15:45:01 +0100

    Add styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :foods, except: [:edit, :update]
-  resources :recipes
+  resources :recipes, except: [:edit, :update] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/general_shopping_list', to: 'foods#general'
+  get '/public_recipes', to: 'recipes#public'
 end
