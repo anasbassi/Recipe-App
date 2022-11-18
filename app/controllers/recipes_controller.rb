ec0314f31ba2f05f998bcd2d519c4851528e4e98
eaesmat <ea.ahmadzai2020@gmail.com>
2022-11-18 18:31:32 +0430

    add food list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,48 +1,53 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all.order(created_at: :desc)
-  end
-
-  def new
-    @recipe = Recipe.new
-  end
-
-  def create
-    @recipe = Recipe.new(recipe_params)
-    @recipe.user_id = current_user.id
-    if @recipe.save
-      redirect_to recipes_path
-    else
-      render :new
-    end
+    @recipes = Recipe.all
   end
 
   def show
     @recipe = Recipe.find(params[:id])
   end
 
-  def edit
-    @recipe = Recipe.find(params[:id])
+  def new
+    @recipe = Recipe.new
   end
 
-  def update
-    @recipe = Recipe.find(params[:id])
-    if @recipe.update(recipe_params)
-      redirect_to @recipe
+  def public
+    @totals = {}
+    @public_recipes = Recipe.where(public: true).order('created_at DESC')
+    @public_recipes.each do |pub|
+      total = 0
+      RecipeFood.where(recipe_id: pub.id).each do |rec_food|
+        total += rec_food.quantity * rec_food.food.price
+      end
+      @totals[pub.name] = total
+    end
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    if @recipe.save
+      flash[:notice] = 'Recipe was saved successfully.'
+      redirect_to recipes_path
     else
-      render :edit
+      flash.now[:alert] = 'There was an error creating the recipe. Please try again.'
+      render :new
     end
   end
 
   def destroy
     @recipe = Recipe.find(params[:id])
-    @recipe.destroy
+    if @recipe.destroy
+      flash[:notice] = 'Recipe was deleted successfully.'
+    else
+      flash.now[:alert] = 'There was an error deleting the recipe.'
+    end
     redirect_to recipes_path
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    params.require(:recipe).permit(:name, :cooking_time, :preparation_time, :description, :public)
   end
 end
--- vendor/bundle/ruby/3.0.0/gems/devise-bootstrap5-0.1.3/lib/devise-bootstrap5.rb
+++
@@ -1,6 +0,0 @@
-require 'rails'
-
-module DeviseBootstrap5
-  class Engine < ::Rails::Engine
-  end
-end
